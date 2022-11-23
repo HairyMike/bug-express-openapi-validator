c5db9c47b16d47eba3be5aff812ff914ed56ac67
Michael Eller <michael.eller@rakuten.com>
2022-11-23 20:16:48 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const OpenApiValidator = require('express-openapi-validator');
+const app = express();
+
+app.use(
+    OpenApiValidator.middleware({
+      apiSpec: './openapi.yaml',
+      validateRequests: true, // (default)
+      validateResponses: true, // false by default
+    }),
+  );
+
+app.get('/test', (req, res) => {
+    res.send('Hello World!')
+});
+
+const port = 3000;
+app.listen(port, async () => {
+    console.log(`Example app listening on port ${port}`)
+});
